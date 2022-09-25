0621a8d3849609419cc89f67008866b425d61e6d
I_am_Mr_Abnormal <106374780+beereshpkambali@users.noreply.github.com>
2022-09-25 22:24:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ'url='youtube.com/channel/UCjXAdU8aMQLvHsRsO5tluAA') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ',url='youtube.com/channel/UCjXAdU8aMQLvHsRsO5tluAA') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
